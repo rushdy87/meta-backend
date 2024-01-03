999eae04a0ef14c8e5218856cda1b7348e5ecb63
Mohammed Alrushdy <mohammed_alrushdy@yahoo.com>
2024-01-04 00:23:33 +0300

    Building API

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -4,26 +4,23 @@ from django.db import models
 
 
 class Booking(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=200)
     no_of_guests = models.IntegerField(default=6)
     booking_date = models.DateTimeField()
 
-    class Meta:
-        verbose_name = 'Booking'
-        verbose_name_plural = 'Booking Records'
-
-    def __str__(self) -> str:
-        return f'{self.name} for {self.number_of_guests} guests on {self.booking_date}'
+    def __str__(self):
+        return self.name
 
 
 class Menu(models.Model):
-    title = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    inventory = models.IntegerField(default=5)
+    title = models.CharField(max_length=200)
+    price = models.IntegerField(null=False)
 
-    class Meta:
-        verbose_name = 'Menu'
-        verbose_name_plural = 'Menu Items'
+    def __str__(self):
+        return self.title
 
-    def __str__(self) -> str:
-        return f'{self.title} : {str(self.price)}'
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
+    featured = models.BooleanField(db_index=True)
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+
+from .models import MenuItem, Booking
+from .serializers import MenuItemSerializer, BookingSerializer
 
 # Create your views here.
 
 
 def index(requist):
     return render(requist, 'index.html', {})
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
